1d0ff9fec5005091c89afdf5ba4a1a5d955d490f
Simon Hohberg <simon.hohberg@mcs-datalabs.com>
2017-11-27 14:08:19 +0100

    Implementing basic recognition

--- src/app/imageUtils.ts
+++ src/app/imageUtils.ts
@@ -1,5 +1,4 @@
 import { otsu } from '../lib/otsu';
-import { Img } from './img';
 import * as numjs from '../lib/numjs';
 
 export class ImageUtils {
@@ -68,5 +67,81 @@ export class ImageUtils {
         let flattened = m.flatten();
         let img = new ImageData(new Uint8ClampedArray(flattened.tolist()), shape[0], shape[1]);
         return img;
+    }
+
+    static drawRect(imgArr, x1, y1, x2, y2, color) {
+        let numChannels = color instanceof Array ? color.length : 1;
+        let imgNumChannels = imgArr.shape.length > 2 ? imgArr.shape[2] : 1;
+        if (imgNumChannels !== numChannels) {
+            throw new Error("Number of color channels of image (" + imgNumChannels + ") and color (" + numChannels + ") does not match");
+        }
+        if (numChannels !== 1 && numChannels !== 3 && numChannels !== 4) {
+            throw new Error("Unsupported number of color channels: " + numChannels + ". Only 1, 3 and 4 are supported.");
+        }
+        for (let x = x1; x <= x2; x++) {
+            if (numChannels === 1) {
+                imgArr.set(y1, x, color);
+                imgArr.set(y2, x, color);
+            } else {
+                imgArr.set(y1, x, 0, color[0]);
+                imgArr.set(y1, x, 1, color[1]);
+                imgArr.set(y1, x, 2, color[2]);
+                imgArr.set(y2, x, 0, color[0]);
+                imgArr.set(y2, x, 1, color[1]);
+                imgArr.set(y2, x, 2, color[2]);
+                if (numChannels === 3) {
+                    imgArr.set(y1, x, 3, 255);
+                    imgArr.set(y2, x, 3, 255);
+                } else {
+                    imgArr.set(y1, x, 3, color[3]);
+                    imgArr.set(y2, x, 3, color[3]);
+                }
+            }
+        }
+        for (let y = y1; y <= y2; y++) {
+            if (numChannels === 1) {
+                imgArr.set(y, x1, color);
+                imgArr.set(y, x2, color);
+            } else {
+                imgArr.set(y, x1, 0, color[0]);
+                imgArr.set(y, x1, 1, color[1]);
+                imgArr.set(y, x1, 2, color[2]);
+                imgArr.set(y, x2, 0, color[0]);
+                imgArr.set(y, x2, 1, color[1]);
+                imgArr.set(y, x2, 2, color[2]);
+                if (numChannels === 3) {
+                    imgArr.set(y, x1, 3, 255);
+                    imgArr.set(y, x2, 3, 255);
+                } else {
+                    imgArr.set(y, x1, 3, color[3]);
+                    imgArr.set(y, x2, 3, color[3]);
+                }
+            }
+        }
+    }
+
+    static drawPoints(imgArr, points, color) {
+        let numChannels = color instanceof Array ? color.length : 1;
+        let imgNumChannels = imgArr.shape.length > 2 ? imgArr.shape[2] : 1;
+        if (imgNumChannels !== numChannels) {
+            throw new Error("Number of color channels of image (" + imgNumChannels + ") and color (" + numChannels + ") does not match");
+        }
+        if (numChannels !== 1 && numChannels !== 3 && numChannels !== 4) {
+            throw new Error("Unsupported number of color channels: " + numChannels + ". Only 1, 3 and 4 are supported.");
+        }
+        for (let p of points) {
+            if (numChannels === 1) {
+                imgArr.set(p.x, p.y, color);
+            } else {
+                imgArr.set(p.y, p.x, 0, color[0]);
+                imgArr.set(p.y, p.x, 1, color[1]);
+                imgArr.set(p.y, p.x, 2, color[2]);
+                if (numChannels === 3) {
+                    imgArr.set(p.y, p.x, 3, 255);
+                } else {
+                    imgArr.set(p.y, p.x, 3, color[3]);
+                }
+            }
+        }
     }
 }
--- src/app/photo.component.ts
+++ src/app/photo.component.ts
@@ -7,25 +7,26 @@ import * as numjs from '../lib/numjs';
   selector: 'photo',
   styleUrls: ['photo.component.css'],
   template: `
-  <div class="wrapper">
-    <div class="overlay">
-        <div class="row semi-transparent grow"></div>
-        <div class="row">
-            <div class="grow semi-transparent"></div>
-            <div #target class="target"></div>
-            <div class="grow semi-transparent"></div>
+    <div class="wrapper">
+        <div class="overlay">
+            <div class="row semi-transparent grow"></div>
+            <div class="row">
+                <div class="grow semi-transparent"></div>
+                <div #target class="target"></div>
+                <div class="grow semi-transparent"></div>
+            </div>
+            <div class="row semi-transparent grow"></div>
         </div>
-        <div class="row semi-transparent grow"></div>
+        <video [hidden]="true" #video autoplay></video>
+        <canvas id="photoCanvas" #canvas></canvas>
+        <canvas #debug1></canvas>
+        <canvas #debug2></canvas>
+        <div class="controls">
+            <button (click)="snap()">Snap</button>
+        </div>
+        <img #img src="assets/test/10747-220.png">
     </div>
-    <video [hidden]="true" #video autoplay></video>
-    <canvas id="photoCanvas" #canvas></canvas>
-    <canvas #inputCanvas></canvas>
-    <div class="controls">
-        <button (click)="snap()">Snap</button>
-    </div>
-    <img #img src="assets/test/10747-220.png">
-  </div>
-  `
+    `
 })
 export class PhotoComponent {
 
@@ -33,7 +34,8 @@ export class PhotoComponent {
 
     @ViewChild('cnnFile') cnnFile;node_modulejimp
     @ViewChild('canvas') canvas;
-    @ViewChild('inputCanvas') inputCanvas;
+    @ViewChild('debug1') debug1;
+    @ViewChild('debug2') debug2;
     @ViewChild('video') video;
     @ViewChild('img') img;
     @ViewChild('target') target;
@@ -64,9 +66,11 @@ export class PhotoComponent {
             .then(cnnJson => {
                 self.cnn = WebCNN.loadNetworkFromJSON(cnnJson);
 
-                self.inputCanvas.nativeElement.width = self.img.nativeElement.width;
-                self.inputCanvas.nativeElement.height = self.img.nativeElement.height;
-                self.context = self.inputCanvas.nativeElement.getContext('2d');
+                self.debug1.nativeElement.width = self.img.nativeElement.width;
+                self.debug1.nativeElement.height = self.img.nativeElement.height;
+                self.debug2.nativeElement.width = self.img.nativeElement.width;
+                self.debug2.nativeElement.height = self.img.nativeElement.height;
+                self.context = self.debug1.nativeElement.getContext('2d');
                 self.context.drawImage(self.img.nativeElement, 0, 0);
 
                 let inputImage = self.context.getImageData(0, 0, self.img.nativeElement.width, self.img.nativeElement.height);
@@ -128,10 +132,135 @@ export class PhotoComponent {
     }
 
     recognize(inputImage: ImageData): Number {
-        let imgMat = numjs.images.read(this.inputCanvas.nativeElement);
+        let imgMat = numjs.images.read(this.debug1.nativeElement);
         let greyMat = numjs.images.rgb2gray(imgMat);
-        let scharr = numjs.images.scharr(greyMat);
-        numjs.images.save(scharr, this.inputCanvas.nativeElement);
+        
+        let binarized = numjs.zeros([greyMat.shape[0], greyMat.shape[1]]);
+        for (let x = 0; x < binarized.shape[1]; x++) {
+            for (let y = 0; y < binarized.shape[0]; y++) {
+                // Ignores alpha
+                let v = ((greyMat.get(y, x, 0) + greyMat.get(y, x, 1) + greyMat.get(y, x, 2)) / 3) > 100 ? 255 : 0;
+                binarized.set(y, x, v);
+            }
+        }
+        // let scharr = numjs.images.scharr(binarized);
+
+        let isWhite = (imgArr, lx, ly) => {
+            return imgArr.get(ly, lx) > 0;
+        }
+        let isOOB = (imgArr, lx, ly) => {
+            return lx >= imgArr.shape[1] || ly >= imgArr.shape[0] || lx < 0 || ly < 0;
+        }
+        let isBorder = (imgArr, lx, ly) => {
+            return imgArr.get(ly-1, lx-1) === 0 
+                || imgArr.get(ly-1, lx) === 0 
+                || imgArr.get(ly-1, lx+1) === 0
+                || imgArr.get(ly,   lx+1) === 0
+                || imgArr.get(ly+1, lx+1) === 0
+                || imgArr.get(ly+1, lx) === 0
+                || imgArr.get(ly+1, lx-1) === 0
+                || imgArr.get(ly,   lx-1) === 0;
+        }
+        let seen = (lx, ly) => {
+            return visited[lx + ',' + ly] === 1;
+        }
+
+        // Horizontal scan line
+        let yMid = Math.round(imgMat.shape[0] / 2);
+        let candidates = [];
+        // Remember all visited points
+        let visited = {}
+        let visitedArr = [];
+        for(let scanX = 0; scanX < imgMat.shape[1]; scanX++) {
+            if (binarized.get(yMid, scanX) === 0) {
+                continue;
+            }
+            let y = yMid;
+            let x = scanX;
+            if (seen(x, y)) {
+                continue;
+            }
+            let minY = yMid;
+            let maxY = yMid;
+            let minX = scanX;
+            let maxX = scanX;
+            let finished = false;
+            // Follow contour
+            let queue = [{ x: x, y: y }];
+            while(queue.length !== 0) {
+                let p = queue.pop();
+                x = p.x;
+                y = p.y;
+                visited[x + ',' + y] = 1;
+                visitedArr.push({x: x, y: y});
+                minX = Math.min(x, minX);
+                maxX = Math.max(x, maxX);
+                minY = Math.min(y, minY);
+                maxY = Math.max(y, maxY);
+                let neighbours = [
+                    {x: x-1, y: y-1},
+                    {x: x,   y: y-1},
+                    {x: x+1, y: y-1},
+                    {x: x+1, y: y},
+                    {x: x+1, y: y+1},
+                    {x: x,   y: y+1},
+                    {x: x-1, y: y+1},
+                    {x: x-1, y: y}
+                ];
+                for (let n of neighbours) {
+                    if (isWhite(binarized, n.x, n.y) && !seen(n.x, n.y) && !isOOB(binarized, n.x, n.y)) {
+                        queue.push(n);
+                    }
+                }
+            }
+            candidates.push([minX, minY, maxX, maxY]);
+            y = yMid;
+            x = minX + 1;
+            // Could enclose other objects
+            while(isWhite(binarized, x, y) && !isOOB(binarized, x, y)) {
+                x++;
+            }
+            x++;
+            finished = true;
+            // ImageUtils.drawPoints(imgMat, visitedArr, [255, 0, 0, 100]);
+        }
+        
+        numjs.images.save(binarized, this.debug2.nativeElement);
+
+        let context = this.debug2.nativeElement.getContext('2d');
+
+        for(let c of candidates) {
+            let w = c[2] - c[0];
+            let h = c[3] - c[1];
+            let ratio = w / h;
+            // Should be portrait rather than landscape rect
+            if (ratio > 1 || ratio < 1/6) {
+                continue;
+            }
+            
+            ImageUtils.drawRect(imgMat, c[0], c[1], c[2], c[3], [0, 255, 0, 255]);
+
+            let patch = context.getImageData(c[0]-5, c[1]-5, w+10, h+10);
+            patch = ImageUtils.resize(patch, 24, 24);
+
+            let result: Array<Array3D> = this.cnn.classifyImages([patch]);
+            let guess = 0;
+            let max = 0;
+            for ( var i = 0; i < 10; ++i )
+            {
+                if ( result[ 0 ].getValue( 0, 0, i ) > max )
+                {
+                    max = result[ 0 ].getValue( 0, 0, i );
+                    guess = i;
+                }
+            }
+            console.log(guess);
+        }
+
+
+
+        numjs.images.save(imgMat, this.debug1.nativeElement);
+
         // this.context.putImageData(ImageUtils.matrixToImg(scharr), 0, 0);
 
         // let inputContext = CanvasRenderingContext2D = this.inputCanvas.nativeElement.getContext('2d');
